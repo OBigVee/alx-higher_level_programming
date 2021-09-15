76b1a054bb3bf4885c329c6ee0d3eecca1db1519
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-15 23:17:10 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv)-1 == 0:
-    print("{:d} arguments.".format(len(sys.argv)-1))
-else:
-    print("{:d} arguments:".format(len(sys.argv)-1))
-    for i in range(1,len(sys.argv)):
-        print("{:d}: {}".format(i,sys.argv[i]))
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)-1 == 0:
+        print("{:d} arguments.".format(len(sys.argv)-1))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        for i in range(1,len(sys.argv)):
+            print("{:d}: {}".format(i,sys.argv[i]))
