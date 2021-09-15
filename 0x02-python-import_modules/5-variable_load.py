28a133810364488a5e2bfd3ec18f6dbf015c155a
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-16 00:10:52 +0100

    all

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5 import a
-    print(a)
+    """
+    print a value from the imported file
+    """
+    import variable_load_5  
+    print("{}".format(variable_load_5.a))
