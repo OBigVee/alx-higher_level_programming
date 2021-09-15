ec714dbfe75244e60e52498683f37b7f923077ab
obigvee <adedejivictor98@gmail.com>
2021-09-15 15:47:16 -0700

    fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     count = 0
-    for i in range(len(sys.argv)):
-        count += sys.argv[i]
-        print("{:d}".format(count))
+    for i in range(len(sys.argv)-1):
+        count += int(sys.argv[i+1])
+    print("{:d}".format(count))
 #     if len(sys.argv)-1 == 0:
 #         print("{:d} arguments.".format(len(sys.argv)-1))
 #     else:
