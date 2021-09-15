e14d3206a5ad0bcd247806e1d576d83dca61fae1
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-15 23:48:29 +0100

    fixx

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,4 @@ if __name__ == "__main__":
     for i in range(len(sys.argv)-1):
         count += int(sys.argv[i+1])
     print("{:d}".format(count))
-#     if len(sys.argv)-1 == 0:
-#         print("{:d} arguments.".format(len(sys.argv)-1))
-#     else:
-#         print("{:d} arguments:".format(len(sys.argv)-1))
-#         for i in range(1,len(sys.argv)):
-#             print("{:d}: {}".format(i,sys.argv[i]))
+
