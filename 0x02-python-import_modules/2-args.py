b39ce1c99d4f84ef707ba9d6053445a3712f2c41
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-15 23:11:43 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,5 @@ if len(sys.argv)-1 == 0:
     print("{:d} arguments.".format(len(sys.argv)-1))
 else:
     print("{:d} arguments:".format(len(sys.argv)-1))
-    for i in range(len(sys.argv)):
-        print("{:d}: {}".format(len(sys.argv)-1,sys.argv[i]))
+    for i in range(1,len(sys.argv)):
+        print("{:d}: {}".format(i,sys.argv[i]))
