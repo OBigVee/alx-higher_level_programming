dc324b251b485b7d5b384e7a53423c49aae1b874
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-18 02:54:46 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-#     for keys, values in a_dictionary.items():
       if key in a_dictionary:
           a_dictionary[key] = value
           return a_dictionary
       else:
           a_dictionary[key] = value
           return a_dictionary
-        
-   
