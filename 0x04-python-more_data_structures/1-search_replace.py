f1054269acab8e08b05bb558f1ade05e6ff1a11d
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-18 01:23:20 +0100

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    _list = []
+    for element in my_list:
+        if search == element:
+            _list.append(replace)
+        else:
+            _list.append(i)
+    return _list
