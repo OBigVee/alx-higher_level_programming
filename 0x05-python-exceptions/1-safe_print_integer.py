d1aa21cf990ac28258594aed4240d746c25faf2b
obigvee <adedejivictor98@gmail.com>
2021-09-27 12:46:47 -0700

    safe print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
-    
-    return
-  
+        try:
+            print("{:d}".format(value))
+            return True
+        except ValueError:
+            return False
