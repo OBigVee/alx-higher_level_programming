8c4c97173bb2da23a50d17a03d074e2af7d7a3ea
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-29 22:02:01 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+""" Define a square class """
+
+
 class Square:
+"""Represent a square."""
 
     def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         return self.size = size
 
     @property
     def size(self):
+        """ size sett """
         return self.__size
 
     @size.setter
-    def size(self,size):
+    def size(self, size):
+        """ set size"""
         if type(size) != int:
             raise TypeError("size must be integer")
         elif size < 0:
