71fe55c2b698d8702556af17ade46e1c46b32e5e
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-29 21:33:55 +0100

    Update 5-sqaure.py

--- 0x06-python-classes/5-sqaure.py
+++ 0x06-python-classes/5-sqaure.py
@@ -8,13 +8,13 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) != int:
+    def size(self, value):
+        if type(value) != int:
             raise TypeError("size must be integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         return self.__size * self.__size
