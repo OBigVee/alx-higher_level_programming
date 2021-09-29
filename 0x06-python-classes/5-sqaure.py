c10d4dc158d73634c029677b7f5f228756fb2f4d
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-29 21:06:00 +0100

    5-s

---
+++ 0x06-python-classes/5-sqaure.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            row = self.__size
+            col = self.__size
+
+            for i in range(row):
+                for j in range(col):
+                    print("#", end="")
+                print("")
+        
