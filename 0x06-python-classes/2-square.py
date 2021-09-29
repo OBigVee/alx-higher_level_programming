2db219637147ad91068988018e4d9ae1b08bac85
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-29 20:33:46 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 class Square:
+    
     def __init__(self, size=0):
-        return self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self,size):
+        """Init"""
         if type(size) != int:
-            raise TypeError("size must be integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size
+#     def __init__(self, size=0):
+#         return self.size = size
+
+#     @property
+#     def size(self):
+#         return self.__size
+
+#     @size.setter
+#     def size(self,size):
+#         if type(size) != int:
+#             raise TypeError("size must be integer")
+#         elif size < 0:
+#             raise ValueError("size must be >= 0")
+#         else:
+#             self.__size = size
