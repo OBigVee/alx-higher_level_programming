5cab3d772fbfb8512328df1aee047d7048e6cd3d
Victor Adedeji <adedejivictor98@gmail.com>
2021-09-29 21:54:56 +0100

    Create 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" Define a class Square """
+
 
 class Square:
-    """Square class"""
-
+    """ Represents a Square """
     def __init__(self, size):
+        """ Initialises new Square
+        Args:
+            size (int): size of side of the square
+        """
         self.__size = size
-
