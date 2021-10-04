6473a9d0db8d1968cd20c4efc84839de4b32b95f
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-04 23:10:57 +0100

    1-rec

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,21 +24,21 @@ class Rectangle:
         return self.__height
       
     @width.setter  
-    def width(self,val):
+    def width(self,value):
         """ width mutator/setter, sets a new width value  """
-        if not isinstance(val, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif val < 0 :
-            raise ValueErrot("width must be >= 0")
+        elif value < 0 :
+            raise ValueError("width must be >= 0")
         else:
-            self.__width = val
+            self.__width = value
             
     @height.setter
-    def height(self,val):
+    def height(self,value):
         """ height mutator/setter, sets a new height value for height """
-        if not isinstance(val, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif val < 0 :
-            raise ValueErrot("width must be >= 0")
+        elif value < 0 :
+            raise ValueError("width must be >= 0")
         else:
-            self.__height = val
+            self.__height = value
