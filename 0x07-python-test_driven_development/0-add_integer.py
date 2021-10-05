9ccfb210fec5513b93615d80c625250e8bb50a3d
obigvee <adedejivictor98@gmail.com>
2021-10-05 12:56:36 -0700

    ship

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,7 @@ def add_integer(a, b=98):
             b (int): second value with a defualt value 98
     Returns :
             sum of a and b
-        """
-    
-   
+      """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)) :
