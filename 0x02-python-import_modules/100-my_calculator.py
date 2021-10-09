29fcad5fd66ec0576eaf43aa47e049962d2ca647
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-09 14:08:26 +0100

    cal py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,17 +2,18 @@
 
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-  
-    if len(sys.argv)-1 != 3:
-        print("Usage:  ./100-my_calculator.py <a> <operator> <b>")
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    operation = {"+": add, "-": sub, "*": mul, "/": div}
-    if not str(sys.argv[2]) in "+-*/":
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    op = sys.argv[2]
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, op, b, operation[op](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
