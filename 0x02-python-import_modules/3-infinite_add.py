b7f08d162968dc680bbf2fc7a89f41559f07d42e
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-09 12:47:10 +0100

    inf py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
+
 if __name__ == "__main__":
     import sys
-    count = 0
-    for i in range(len(sys.argv)-1):
-        count += int(sys.argv[i+1])
-    print("{:d}".format(count))
 
+    count = len(sys.argv) - 1
+    result = 0
+
+    for i in range(count):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
