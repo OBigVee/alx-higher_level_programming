122a12199e9d9bdb28f1b24c0fe0e75b81bdba61
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-11 22:14:50 +0100

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""function returns True if the object is and instance of, or if the object is an instance\
+"""function returns True if the object is and instance of, or if the object is an instance
     of a class that inherits form the specified class; otherwise Flase
 """
 
 
 def is_kind_of_class_(obj, a_class):
-    """returns True if obj is an isinstance of a_class"""
+    """Returns:
+            True if obj is an isinstance of a_class
+    """
+    
     if isinstance(obj, a_class):
         return True 
     else:
