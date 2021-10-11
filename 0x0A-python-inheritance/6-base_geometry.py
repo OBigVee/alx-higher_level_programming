f302c6507e2f52ae21dc93c96966aca581181126
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-11 22:45:03 +0100

    improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define a empty class BaseGeometry based on `5-base_geometry.py`"""
+
+
+class BaseGeometry:
+    """define a pub method area()"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
