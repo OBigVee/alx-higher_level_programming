8d6849c8fda3e0679d1783dfdb02e1736fd95d8c
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-11 23:49:32 +0100

    square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -58,4 +58,4 @@ class Square(Rectangle):
     
     def __str__(self):
         """return, the square description"""
-       return("[Square] {}/{}".format(self.__size,self.__size))  
+        return("[Square] {}/{}".format(self.__size,self.__size)) 
