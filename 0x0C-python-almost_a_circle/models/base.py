5edbb29bd212fde496299699ae350a54788c4037
Victor Adedeji <adedejivictor98@gmail.com>
2021-10-18 15:45:10 +0100

    base..

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-
+"""Base """
+class Base:
+  """
+  __bn_objects:(private attr)
+  """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
