0560bc325d9b2ae5a6bac409faf10d0e11ba6984
OBigVee <adedejivictor98@gmail.com>
2021-10-18 17:11:39 +0100

    First Rectangle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,11 +4,14 @@ class Base:
   """
   __bn_objects:(private attr)
   """
-    __nb_objects = 0
+  __nb_objects = 0
     
-    def __init__(self, id=None):
-        if id != None:
+  def __init__(self, id=None):
+    if id != None:
             self.id = id
-        else:
+    else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+    
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Rectangle class inherits from Base class"""
+from base import Base
+
+class Rectangle(Base):
+    """
+    class information
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x  = x
+        self.y = y
+    
+
+    @property
+    def width(self):
+        """ """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ """
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        """
+        self.__height = value
+    
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """ """
+        self.__x = value
+    
+    @property
+    def y(self):
+        """ """
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """ """
+        self.__y = value
