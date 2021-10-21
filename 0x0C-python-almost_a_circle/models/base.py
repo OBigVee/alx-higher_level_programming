066da8bfbfe1fa8716b66fe59c10fe96042b537e
OBigVee <adedejivictor98@gmail.com>
2021-10-21 11:26:46 +0100

    PEP8 compliance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Base """
+
+
 class Base:
-  """
-  __bn_objects:(private attr)
-  """
-  __nb_objects = 0
-    
-  def __init__(self, id=None):
-    if id != None:
+    """
+    __bn_objects:(private attr)
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
             self.id = id
-    else:
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
-    
