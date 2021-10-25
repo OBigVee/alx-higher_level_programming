c58d7e3572ab025ff10fd8415bfc3aa1d1e4e358
OBigVee <adedejivictor98@gmail.com>
2021-10-25 11:18:28 +0100

    Square Size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
     def __init__(self,size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
         """ Note that a square height and width are equal"""
-        self.size = size
+        self.size = siz
         
 
     @property
