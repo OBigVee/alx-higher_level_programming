e1baef5db5419554971f798cb491952a996fe7ca
OBigVee <adedejivictor98@gmail.com>
2021-10-25 16:42:37 +0100

    dict to JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base """
 
+import json
+
 
 class Base:
     """
@@ -15,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """parse data to JSON"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
