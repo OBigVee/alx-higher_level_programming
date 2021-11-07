afac978d975ee39d681b001363eafa542930b3ae
Victor Adedeji <adedejivictor98@gmail.com>
2021-11-07 18:23:17 +0100

    write 2 file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-  writes a string to a text file(UTF8) and returns the number of characters written
-  """
+""" writes a string to a text file(UTF8) and returns the number of characters written """
 
 
 def write_file(filename="", text=""):
   """ read file """
     with open(filename, mode='w', encoding='utf-8') as file:
-        writeText2File = file.write(text)
-        return writeText2File
+        #writeText2File = file.write(text)
+        return file.write(text)
