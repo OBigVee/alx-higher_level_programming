e73db9d589a27800af832e5869fca3cddb039440
obigvee <adedejivictor98@gmail.com>
2021-12-15 16:42:28 -0800

    JS

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+console.log("C isfun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
+
---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if(arguments.length = 0){
+	console.log("No arguments");
+}
+else if(arguments = 1){
+	console.log("Argument found");
+}
+else if(arguments > 1){
+	console.log("Arguments found");
+}
+
