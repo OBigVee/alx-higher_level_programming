b9003e88d70d480828a893e6aa1cf81bf539f307
Victor Adedeji <adedejivictor98@gmail.com>
2021-12-29 20:30:58 +0100

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const arguments = process.argv.length -2 ;
-if(arguments === 0){
-	console.log("No arguments");
-}
-else if(arguments === 1){
-	console.log("Argument found");
-}
-else if(arguments > 1){
-	console.log("Arguments found");
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
