799cc7ead3072177a51c8cf14cbce99ed7b8f418
Victor Adedeji <adedejivictor98@gmail.com>
2022-02-04 23:22:58 +0100

    feat:
    added a function that creates an object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function creates an object from JSON file """
+
+import json
+
+def load_from_json_file(filename):
+  """ filename: """
+    with open(filename, "r") as obj_file:
+        return json.load(obj_file)
+        
+       
