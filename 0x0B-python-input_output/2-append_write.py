eb4c646c52094a2eff81ebc78fd54a21913b9bf7
Victor Adedeji <adedejivictor98@gmail.com>
2022-02-04 18:44:26 +0100

    docs:
    re-write comments with code

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""functions append a string at the end of a text file (UTF8) and returns the number of char added"""
+"""
+    functions append a string at the end of a text file (UTF8) 
+    and returns the number of char added
+"""
 
 
 def append_write(filename="", text=""):
     """append file"""
     with open(filename, mode='a', encoding='utf-8') as file:
         append2File = file.write(text)
-        return append2File
+    return append2File
