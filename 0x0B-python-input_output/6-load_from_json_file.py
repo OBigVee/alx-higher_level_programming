7f5753c938550e7d0842c8da5a2d36a545dd332f
obigvee <adedejivictor98@gmail.com>
2022-02-04 14:48:50 -0800

    refactor
    removed redundant lines of code

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """function creates an object from JSON file """
 
+
 import json
 
+
+
 def load_from_json_file(filename):
-  """ filename: """
+    """ filename: """
     with open(filename, "r") as obj_file:
         return json.load(obj_file)
         
