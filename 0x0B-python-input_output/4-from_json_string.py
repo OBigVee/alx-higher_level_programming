8df5bef5e94fad9d0bdd43507e47097793061c4a
Victor Adedeji <adedejivictor98@gmail.com>
2022-02-04 20:03:14 +0100

    feat:
    functions returns an object (primitive data type) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function returns an object (data type) rep by a JSON string"""
+
+import json
+def from_json_string(my_str):
+    """ my_str: JSON string  """
+    reponse = json.loads(my_str)
+    return response
