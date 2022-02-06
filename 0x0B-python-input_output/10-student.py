ba86a672d39934ec4635641ccc981127204f0876
obigvee <adedejivictor98@gmail.com>
2022-02-06 04:58:45 -0800

    fix
    fix and correct undefined variables

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,21 +5,22 @@
 
 
 class Student:
-    """ initialisation"""
+    """initialisation"""
+
     def __init__(self, first_name, last_name, age):
         """ """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
-        """ to_json: retrieves a dict representation of a Student 
-        instance (same as 8-class_to_json.py) """
-       if attrs is not None and all(isinstance(items, str) for item in attrs):
-           dict = {}
-           for key,pair in self.__dict__.items():
-               if key in attrs:
-                   dict[key] = pair
-           return dict
-      else:
-          return self.__dict__
+        """to_json: retrieves a dict representation of a Student
+        instance (same as 8-class_to_json.py)"""
+        if attrs is not None and all(isinstance(item, str) for item in attrs):
+            dict = {}
+            for key, pair in self.__dict__.items():
+                if key in attrs:
+                    dict[key] = pair
+            return dict
+        else:
+            return self.__dict__
