0310160bbbf0558ee47b5bc6cac1c728ea4e1404
OBigVee <adedejivictor98@gmail.com>
2022-02-23 13:32:18 +0100

    refactor:
    Refactor 1-hbtn_header.py to pep8 compliance

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-    script takes in a url, send a request to the url and display the of the X-Request_Id variable
+    script takes in a url, send a request to the url and display the of
+    the X-Request_Id variable
 """
 
 from urllib import response
 
 
 if __name__ == "__main__":
-    import urllib.request 
+    import urllib.request
     import sys
 
     the_url = sys.argv[1]
