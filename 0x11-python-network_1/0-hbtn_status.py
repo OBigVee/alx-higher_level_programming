680c76afa6eabd67cc064c8bf8fb7361fdf5708a
OBigVee <adedejivictor98@gmail.com>
2022-02-23 11:47:06 +0100

    feat:
    Add a script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,14 +5,11 @@
 if __name__ == "__main__":
     import urllib.request as request
 
-    #the_url = request)
+    # the_url = request)
 
     with request.urlopen("https://alx-intranet.hbtn.io/status") as req:
         html = req.read()
         print("Body response:")
         print("\t - type: {}".format(type(html)))
         print("\t - content: {}".format(html))
-        print("\t - utf8 content: {}".format(html.decode('utf-8')))
-
-
-
+        print("\t - utf8 content: {}".format(html.decode("utf-8")))
