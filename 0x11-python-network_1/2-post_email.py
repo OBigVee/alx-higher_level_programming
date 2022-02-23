4323ef080f7ffceacb68b26e4985f07350c18d3b
OBigVee <adedejivictor98@gmail.com>
2022-02-23 14:45:06 +0100

    feat
    - Add 2-post_-email.py script
    - 2-post_email.py takes in a URl an an Email and send POST request to the parsed URL with the email as a parameter.
    - Decode the body of the response in utf-8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ script takes in a URL and Email, send a Post request
-    to the passed URL with the email as a parameter, and 
+to the passed URL with the email as a parameter, and
     display the body of the response(decoded in `utf-8`)
 """
 from urllib import request, parse
@@ -8,10 +8,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    value = {"email" : sys.argv[2]}
+    value = {"email": sys.argv[2]}
 
     data = parse.urlencode(value)
-    data = data.encode('utf-8')
+    data = data.encode("utf-8")
     req = request.Request(url, data)
     with request.urlopen(req) as resp:
-        output = resp.read().decode('utf-8')
+        output = resp.read().decode("utf-8")
