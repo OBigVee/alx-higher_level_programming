849334fda64c11155a10cfb04a36593544e1bebf
OBigVee <adedejivictor98@gmail.com>
2022-02-24 13:32:10 +0100

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server:
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
