7b4cdec83f62f09f1ece0b23a67aff86a5908153
OBigVee <adedejivictor98@gmail.com>
2022-03-28 11:22:36 +0100

    feat:
    search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,21 @@
 #!usr/bin/python3
 
 """ Search and update"""
+
+
 def append_after(filename="", search_string="", new_string=""):
     content = []
-    with open(filename, 'r', encoding="utf-8") as fileobject:
+    with open(filename, "r", encoding="utf-8") as fileobject:
         content = fileobject.readlines()
 
         idx = 0
-        #print(content)
+        # print(content)
 
         while idx < len(content):
             if search_string in content[idx]:
-                #print(content[idx])
-                content[idx:idx+1] = [content[idx], new_string]
-                #print(content) 
+                # print(content[idx])
+                content[idx:idx + 1] = [content[idx], new_string]
+                # print(content)
                 idx += 1
             idx += 1
     with open(filename, "w", encoding="utf-8") as file:
