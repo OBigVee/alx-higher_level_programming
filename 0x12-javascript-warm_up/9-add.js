b98c83d238644f7446fed80bdbe8e36df7033aa3
OBigVee <adedejivictor98@gmail.com>
2022-04-05 00:46:08 +0100

    Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+ //script prints the addition of  2 ints
+
+function add(a, b) {
+    const args = require('process').argv;
+    a = parseInt(args[2]);
+    b = parseInt(args[3]);
+
+    console.log(a + b);
+}
+
+add();
