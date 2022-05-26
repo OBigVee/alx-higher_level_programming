9c26fb16a562300a36097ef94a60bedd1cd229c1
OBigVee <adedejivictor98@gmail.com>
2022-05-26 14:36:39 +0100

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+/**
+ * nbOccurence returns the number of occurrences in a list
+ * @param list
+ * @param searchElement
+ * @returns {number}
+ */
 exports.nbOccurences = function (list, searchElement) {
     let count = 0;
     for(let i = 0; i < list.length; i++){
