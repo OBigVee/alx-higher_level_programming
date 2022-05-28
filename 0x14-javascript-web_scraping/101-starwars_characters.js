38af27b03ac0e8d9f36e35014e49c9f55f12e054
OBigVee <adedejivictor98@gmail.com>
2022-05-28 17:14:58 +0100

    feat:
    - Who was palying in this movie?
    -script list names of the character in the Star Wars episode ID
    - names are listed in Right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/**
+ * Script prints all chracters of a Start Wars movie:
+ * -> The first arg is the movie ID -example: 3 = "Return of the Jedi"
+ * -> Display one character name by line in the same order of the list "characters" in the /films/ response
+ * -> you must use the Star Wars API
+ * -> You must use the module request
+ */
+const request = require('request')
+
+const url = 'https://swapi-api.hbtn.io/api/films/'
+
+if (process.argv.length > 2) {
+    const episodeID = process.argv[2]
+    request(`${url}${episodeID}`, function (error, response, body) {
+        if (error) throw error
+        else if (body) {
+            const charURL = JSON.parse(body).characters;
+            //console.log(charURL);
+            const charName = charURL.map(
+                eachUrl => new Promise((resolve, reject)=>{
+                    request(eachUrl,function (err, response, body){
+                        if(err) throw reject(err);
+                        resolve(JSON.parse(body).name);
+                        //console.log(resolve(JSON.parse(body).name));
+                    });
+                }));
+            console.log(charName);
+            Promise.all(charName)
+                .then(names => console.log(names.join("\n")))
+                .then(error => console.log(error))
+        }
+    })
+}
