146299fca84d715c981a417cdd56ac3e58e2e0d6
OBigVee <adedejivictor98@gmail.com>
2022-05-29 02:31:16 +0100

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
-#!/bin/node
+#!/bin/bash
 # Bash script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 # A variable email must be sent with the value test@gmail.com
 # A variable subject must be sent with the value I will always be here for PLD
