981d69d8659bcd3f40256db6d662ae99726af903
OBigVee <adedejivictor98@gmail.com>
2022-12-07 16:57:22 +0100

    task 1 completed :heavy_check_mark:
    - added 1-last_digit.py file
    program print the last digit of a random number and state some information about them.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,16 @@
 import random
 
 number = random.randint(-10000, 10000)
-if (number < 0):
+if number < 0:
     last_digit = int(repr(number)[-1]) * -1
 else:
     last_digit = int(repr(number)[-1])
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit}  and is {last_digit}")
+    print(f"Last digit of {number} is {last_digit} and is {last_digit}")
 elif (last_digit < 6) and (last_digit != 0):
-    #if last_digit < 0:
+    # if last_digit < 0:
     #    print(f"Last digit of {number} is {-last_digit} and is less than 6 and not 0")
-    #else:
+    # else:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
