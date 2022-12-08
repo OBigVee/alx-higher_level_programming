3590fb6577a887cdd1b8ba29d37cbc5914637a26
Victor Adedeji <adedejivictor98@gmail.com>
2022-12-08 18:16:08 +0100

    task 5 :wrench:

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -9,8 +9,12 @@ You can only use one loop in your code
 You are not allowed to store numbers or strings in a variable
 You are not allowed to import any module
 """
+"""
 for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
         print("{:02}, ".format(number), end='')
+"""
+for i in range(100):
+    print('{:02d}{:s}'.format(i, ', ' * (i < 99) + '\n' * (i == 99)), end='')
