c70ab130b0ae6f6a149a32a576306aff168a7150
OBigVee <adedejivictor98@gmail.com>
2022-12-08 05:45:02 +0100

    task 4 completed :heavy_check_mark:

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+program that prints all numbers from 0 to 98 in decimal and in hexadecimal (as in the following example)
+
+You can only use one print function with string format
+You can only use one loop in your code
+You are not allowed to store numbers or strings in a variable
+You are not allowed to import any module
+"""
+[print(f"{i} = {hex(i)}") for i in range(99)]
