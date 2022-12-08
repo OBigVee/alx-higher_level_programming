fd9e82f26624f1fbd8e33eae168e86affcdf707d
OBigVee <adedejivictor98@gmail.com>
2022-12-08 06:20:38 +0100

    task 5 completed :heavy_check_mark:

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+programs print numbers from 0 to 98
+Numbers must be separated by ,, followed by a space
+Numbers should be printed in ascending order, with two digits
+The last number should be followed by a new line
+You can only use no more than 2 print functions with string format
+You can only use one loop in your code
+You are not allowed to store numbers or strings in a variable
+You are not allowed to import any module
+"""
+for i in range(99 + 1):
+    if i == 99:
+        print("{}".format(i), end="")
+    else:
+        print("{:02}".format(i), end=", ")
