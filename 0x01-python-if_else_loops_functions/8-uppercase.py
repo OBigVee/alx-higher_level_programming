712567d9ec02f23b504d914e83f996abac05a749
Victor Adedeji <adedejivictor98@gmail.com>
2022-12-08 18:33:32 +0100

    task 8 :wrench:

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,7 @@
 """
 function prints a string in uppercase followed by a new line
 """
+"""
 def uppercase(str):
     for i in range(len(str)):
         uni_code = ord(str[i])
@@ -9,3 +10,16 @@ def uppercase(str):
             uni_code -= 32
         print("{}".format(chr(uni_code)), end="")
     print()
+"""
+def uppercase(str):
+    '''
+    Prints a string, converting lowercase characters to uppercase
+    Parameters:
+    str (str): The string to print
+    '''
+    txt = ''.join(str) + '\n'
+    for i in range(len(txt)):
+        c = ord(txt[i])
+        is_lower = (c >= ord('a')) and (c <= ord('z'))
+        offset = (1 << 5) if is_lower else 0
+        print('{:c}'.format(c - offset), end='')
