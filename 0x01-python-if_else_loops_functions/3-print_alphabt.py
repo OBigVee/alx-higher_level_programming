ef1e6815b3685e4c128bd477ee9a9e41d4d24e04
OBigVee <adedejivictor98@gmail.com>
2022-12-08 05:30:21 +0100

    task 3 completed :heavy_check_mark:

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+program prints ASCII alphabet int lowrcase not followed by a new line
+print all the letters execept q and e
+You can only use one print function with string format
+You can only use one loop in your code
+You are not allowed to store characters in a variable
+You are not allowed to import any module
+"""
+for i in range(26):
+    if ord("a") + i != 101 and ord("a") + i != 113:
+        print(f"{ord('a') + i:c}", end="")
+    else:
+        continue
