11004baf75fd3a30e07fb298ac0163cdfffcc459
OBigVee <adedejivictor98@gmail.com>
2022-12-10 22:39:18 +0100

    task 6 completed :heavy_check_mark:

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" """
+if __name__ == "__main__":
+    import sys
+    import calculator_1
+    from calculator_1 import add, sub, mul, div
+
+    n_arg = len(sys.argv)
+
+    if n_arg == 4:
+
+        a = sys.argv[1]
+        b = sys.argv[3]
+        operators = [("+", add), ("-", sub), ("*", mul), ("/", div)]
+
+        for op in operators:
+            if sys.argv[2] == op[0]:
+                result = op[1](a, b)
+                print("{:} {} {:} = {:}".format(a, op[0], b, result))
+                sys.exit()
+        print("Unknown operator. Available operators: {:s}".format("+, -, * and /\n"))
+        sys.exit(1)
+    else:
+        print(
+            "Usage: {} <a> <operator> <b>\n".format(
+                sys.argv[0],
+            )
+        )
+        sys.exit(1)
