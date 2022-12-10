46bff3c22b21231f4cab5e33a6fbf62759af710c
OBigVee <adedejivictor98@gmail.com>
2022-12-10 07:07:59 +0100

    task 2 completed :heavy_check_mark:

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,10 @@ if __name__ == "__main__":
     import sys
 
     n_arg = len(sys.argv) - 1
-    print("{:d} argument{:s}{:s}".format(n_arg, "s" * (n_arg != 1),
-        ":" if n_arg > 0 else "."))
-    #if n_arg == 1:
-    #    print("{:d} arguments.".format(n_arg - 1))
-    #elif n_arg == 2:
-    #    print("{:d} argument:".format(n_arg - 1))
-    #    print("{:d} : {}".format(n_arg - 1, sys.argv[n_arg - 1]))
-    #elif n_arg > 1:
-    #    print("{:d} arguments:".format(n_arg - 1))
+    print(
+        "{:d} argument{:s}{:s}".format(
+            n_arg, "s" * (n_arg != 1), ":" if n_arg > 0 else "."
+        )
+    )
     for arg in range(1, n_arg + 1):
         print("{:d} : {:s}".format(arg, sys.argv[arg]))
