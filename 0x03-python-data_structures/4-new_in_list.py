a6ee8c5f3fdfa34d22a14ca4eac6fabff4712855
OBigVee <adedejivictor98@gmail.com>
2022-12-15 00:45:24 +0100

    task 4 completed :heavy_check_mark:
    - added 4-new_in-list.py file
    - function replaces an element in a list at a specific position without modifying the original list
    - added 4-main.py test case file

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+function replaces an element in a list at a specific position without
+modifying the original list
+"""
+
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0:
+        return new_list
+    elif idx > len(new_list):
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
