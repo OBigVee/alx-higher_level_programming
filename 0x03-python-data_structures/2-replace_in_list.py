b01cf227c3c2345c4650308bebd2384b4e6bcc03
OBigVee <adedejivictor98@gmail.com>
2022-12-14 23:41:51 +0100

    task 2 completed :heavy_task_mark:
    - added 2-replace_in_list.py file
    - function replaves an element of a list at specific position
    - added 2-main.py test case file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-""" """
-
+"""
+function replaces an element of a list at a specific posistion
+"""
+def replace_in_list(my_list, idx, element):
+    if(idx < 0):
+        return (my_list)
+    elif(idx > len(my_list)):
+        return (mylist)
+    else:
+        my_list[idx] = element
+        return my_list
