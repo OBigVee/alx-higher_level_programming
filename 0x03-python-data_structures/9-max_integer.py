7967bc9380a3512ce01aeff3f3d040d835353859
OBigVee <adedejivictor98@gmail.com>
2022-12-15 04:08:23 +0100

    task 9 completed :heavy_check_mark:
    - added 9-max_integer.py file
    - function finds the biggest integer of a list
    - added 9-main.py test case file

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-
-    t_max = 0
-    
-    for i in range(len(list_len)):
-        if my_list[i] >= t_max:
-            t_max = my_list[i]
-    return t_max
+    _max = 0 if len(my_list) == 0 else my_list[0]
+    for i in my_list:
+        _max = i if i >= _max else _max
+    return None if len(my_list) == 0 else _max
