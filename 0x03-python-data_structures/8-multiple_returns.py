47c192e2dba210ab461d8cfc018e09da5a2a67ea
OBigVee <adedejivictor98@gmail.com>
2022-12-15 03:28:11 +0100

    task 8 completed :heavy_check_mark:
    - added 8-multiple_returns.py file
    - function returns a tuple with len of a string and its first character
    - added 8-main.py test case file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" function returns a tuple with lenght of a string
+and its first character"""
+
+
+def multiple_returns(sentence):
+    if sentence == "":
+        return None
+    else:
+        t_len = len(sentence)
+        char = sentence[0]
+        return (t_len, char)
