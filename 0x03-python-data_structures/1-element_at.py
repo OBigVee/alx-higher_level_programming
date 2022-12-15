9f0fa4aaa9c310678e6057da2032698ff5160cce
OBigVee <adedejivictor98@gmail.com>
2022-12-15 01:04:50 +0100

    task 1 completed :heavy_check_mark:
        - added 1-element_at.py file
        - function retrieves an element from a list
        - added 1-main.py test case file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,4 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
-        return my_list[idx]
+    return None if idx < 0 or idx >= len(my_list) else my_list[idx]
