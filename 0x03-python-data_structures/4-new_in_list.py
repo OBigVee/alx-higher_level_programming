b4747eb796a662b2e773741805311acdcfad17a9
OBigVee <adedejivictor98@gmail.com>
2022-12-15 01:22:47 +0100

    task 4 :wrench:

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,10 +7,6 @@ modifying the original list
 
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if idx < 0:
-        return new_list
-    elif idx > len(new_list):
-        return new_list
-    else:
+    if idx >= 0 and idx < len(new_list):
         new_list[idx] = element
         return new_list
