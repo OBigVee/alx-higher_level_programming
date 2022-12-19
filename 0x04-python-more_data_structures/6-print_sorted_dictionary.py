c267e15bdfc9f004229952b05d37044898fad536
OBigVee <adedejivictor98@gmail.com>
2022-12-19 23:44:23 +0100

    task 6 completed :wrench:

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,4 +6,4 @@ function prints a dictionary by ordered keys
 
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
-        print("{:s}:{}".format(key, a_dictionary[key]))
+        print("{:s}: {}".format(key, a_dictionary[key]))
