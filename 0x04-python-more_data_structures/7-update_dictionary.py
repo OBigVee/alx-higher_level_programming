da7ab8b8c5193961f17042afe0b6a51f54b46ad8
OBigVee <adedejivictor98@gmail.com>
2022-12-19 23:45:43 +0100

    task 7 completed :heavy_check_mark:

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,6 +6,5 @@ def update_dictionary(a_dictionary, key, value):
     if a_dictionary is None:
         return None
 
-    if key not in a_dictionary.keys():
-        a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
