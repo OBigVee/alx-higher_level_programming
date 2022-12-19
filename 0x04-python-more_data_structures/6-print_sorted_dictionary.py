b7260f2cbe86edd0856077e40b11c17fcc74ee9a
OBigVee <adedejivictor98@gmail.com>
2022-12-19 23:20:02 +0100

    task 6 completed :heavy_check_mark:

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function prints a dictionary by ordered keys
+"""
+
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{:s}:{}".format(key, a_dictionary[key]))
