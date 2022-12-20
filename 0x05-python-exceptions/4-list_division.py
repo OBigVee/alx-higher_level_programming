c072fc70da0d251f50ea2cebc0940434954178e7
OBigVee <adedejivictor98@gmail.com>
2022-12-20 18:04:06 +0100

    task 4 completed :heavy_check_mark:

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    '''
+    Divides the elements of 2 lists with the same index
+    Parameters:
+    my_list_1 (list): The first list
+    my_list_2 (list): The second list
+    list_length (int): The length of the items to divide
+    Returns:
+    A new list consisting of the result of the divisions
+    '''
     new_list = []
     for i in range(list_length):
         result = 0
         try:
-            result = my_list_1[i] / my_list_2[i]
+            a, b = (my_list_1[i], my_list_2[i])
+            result = a / b
+        except TypeError:
+            print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
-        except (TypeError, ValueError):
-            print("Wrong type")
         except IndexError:
             print("out of range")
         finally:
