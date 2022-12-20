7f4a7c3c1db6a1f0c82492f0be321521ea1a9113
OBigVee <adedejivictor98@gmail.com>
2022-12-20 17:56:27 +0100

    task 4 completed :heavy_check_mark:

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,6 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
