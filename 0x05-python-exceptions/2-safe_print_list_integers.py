5441f265f4547284033bb9bdac50cc365f6aba5d
OBigVee <adedejivictor98@gmail.com>
2022-12-20 17:05:12 +0100

    task 2 completed :heavy_check_mark:

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """function prints the first x element of a list and only integers
 y_list can contain any type (integer, string, etc.)
-All integers have to be printed on the same line followed by a new line - other type of value in the list must be skipped (in silence).
+All integers have to be printed on the same line followed
+- by a new line - other type of value in the list must be skipped (in silence).
 x represents the number of elements to access in my_list
-x can be bigger than the length of my_list - if it’s the case, an exception is expected to occur
+x can be bigger than the length of my_list - if it’s the case,
+- an exception is expected to occur
 Returns the real number of integers printed
 You have to use try: / except:
 You have to use "{:d}".format() to print an integer
