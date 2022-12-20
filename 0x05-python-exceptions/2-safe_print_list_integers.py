aaf7601a10947e77f1ddcb7ee28a12f3a1a0b3ee
OBigVee <adedejivictor98@gmail.com>
2022-12-20 17:01:46 +0100

    task 2 completed :heavy_check_mark:

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -18,7 +18,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
+        except ValueError:
             continue
+	except TypeError:
+	    continue
     print()
     return count
