2217f5d2354e8f03f4fdfd3ab4c627982d49c56a
OBigVee <adedejivictor98@gmail.com>
2022-12-20 18:25:25 +0100

    task 7 completed :heavy_check_mark:
    - adde 100-safe_print_integer_err.py file

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(e.args[0])
+        sys.stderr.write("\n")
+        return False
