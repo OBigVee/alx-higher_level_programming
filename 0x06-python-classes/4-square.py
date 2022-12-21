21a4c22b7f090783d18453a7101db8139c93a6da
OBigVee <adedejivictor98@gmail.com>
2022-12-21 14:53:09 +0100

    task 4 completed :heavy_check_mark:
    - added 4-square.py file
    - file contains a defined Square class
    - add 4-main.py test case file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Square class defined based on '3-square.py'"""
+
+
+class Square:
+    """Represents a 2D polygon with 4 side equal to each other
+    """
+    def __init__(self, size=0):
+        """ initialize Square with size
+
+        Args:
+            size (int): the size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Computes the area of square.
+
+        Returns:
+            int: the computed area
+        """
+        return self.__size ** 2
