fcdc5d7921a54439a04af3a138177cfe8c9213c1
OBigVee <adedejivictor98@gmail.com>
2022-12-21 14:15:47 +0100

    task 3 completed :heavy_check_mark:
    - added 3-square.py file
    - file contains a defined Square class
    - add 3-main.py test case file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,7 +18,7 @@ class Square:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
-    
+
     def area(self):
         """Computes the area of square.
 
