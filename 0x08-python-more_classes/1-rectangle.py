f16960e2a1ed9da688cf2832beea894615c8008e
OBigVee <adedejivictor98@gmail.com>
2023-01-02 21:33:56 +0100

    task 1 completed :heavy_check_mark:

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Rectangle Class"""
+
+
+class Rectangle:
+    """ """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width modifier
+        Args:
+            value(int): value of the width
+        Return:
+            the new modified value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height modifier
+        Args:
+            value(int): value of the height
+        Return:
+            the new modified value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
