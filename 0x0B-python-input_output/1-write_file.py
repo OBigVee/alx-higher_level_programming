e141675d43d20204de552a876ff3c1071e53e5a4
OBigVee <adedejivictor98@gmail.com>
2023-01-10 13:52:23 +0100

    task 1 completed :heavy_check_mark:

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr//bin/python3
+"""function writes a string to a text file(UTF8) and returns the number of the characters written"""
+
+
+def write_file(filename="", text=""):
+    """
+    Args:
+        filename: name of the file to process
+        text: text to be append to the file filename
+    """
+    with open(filename, mode="w", encoding="utf-8") as file_obj:
+        data = file_obj.write(text)
+        return data
