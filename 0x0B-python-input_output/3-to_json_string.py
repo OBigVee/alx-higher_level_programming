d139505ef4f118af3eb4089ee249bc9f77a3b89f
OBigVee <adedejivictor98@gmail.com>
2023-01-10 14:19:22 +0100

    task 3 completes :heavy_check_mark:

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function returns the json representation of an object(string)"""
+import json
+
+def to_json_string(my_obj):
+    """
+    Arg:
+        my_obj(string)
+    """
+    return json.dumps(my_obj)
