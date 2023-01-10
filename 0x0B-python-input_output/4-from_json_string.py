cb1dae0391e862f442a373690aa9fed4cb644cd4
OBigVee <adedejivictor98@gmail.com>
2023-01-10 14:27:12 +0100

    task 4 completed :heavy_check_mark:

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""function returns an object(python DS) rep by a json
+    string:
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Args:
+        my_str:json data to be converted to python 
+                data structure
+    """
+    return json.loads(my_str)
