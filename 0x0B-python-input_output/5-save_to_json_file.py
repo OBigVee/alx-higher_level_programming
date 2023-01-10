350d35b1ff7c751c88c2ba0bc6c8ae7450e3de3b
OBigVee <adedejivictor98@gmail.com>
2023-01-10 17:22:46 +0100

    :white_check_mark: task 5 completed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,14 +2,14 @@
 """function writes an object to a text file, using a
     json repersentation
 """
-
-
 import json
+
 def save_to_json_file(my_obj, filename):
     """Args:
-        my_obj: object to be written to text file filename
+        my_obj: object to be written to text file filenamere
         filename: file to be written to
     """
-    with open(filename, mode="w") as f_obj:
-        json_file = json.dumps(my_obj)
-    return json_file
+    with open(filename, mode="w", encoding="utf-8") as f_obj:
+        json.dump(my_obj, f_obj)
+        #f_obj.write(JSONEncoder().encode(my_obj))
+    
