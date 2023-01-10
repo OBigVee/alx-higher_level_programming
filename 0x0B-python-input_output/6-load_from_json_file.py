2a2d00517763faf679ac2d5193cf5a664a96e2a0
OBigVee <adedejivictor98@gmail.com>
2023-01-10 18:18:34 +0100

    :wrench: task completed

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,9 +7,9 @@ import json
 
 def load_from_json_file(filename):
     """Args:
-        filename: 
+    filename:
     """
     with open(filename, encoding="utf-8") as f_obj:
         deserialize_obj = json.load(f_obj)
-    
+
     return deserialize_obj
