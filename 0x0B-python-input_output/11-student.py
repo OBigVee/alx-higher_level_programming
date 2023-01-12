fec86d928fe62d4f3dddfbaa2d8e41b362020359
OBigVee <adedejivictor98@gmail.com>
2023-01-12 12:06:02 +0100

    :white_check_mark: task 11 completed
    - added 11-student.py file
    - file contains a class with 2 methods
    - reload_from_json method replaces all atrributes of the Student class instance
    - added 11-main.py test case file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """ class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = second_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ method retrieves a dict rep of a Student
+        instance (same as 8-class_to_json.py)
+        """
+        if attrs is not None: 
+            if all(isinstance(i, str) for i in attrs):
+                dict = {}
+                for k,v in self.__dict__.items():
+                    if k in attrs:
+                        dict[k] = v
+                return dict
+        else:
+            return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """method replaces all attributes of the Student instance"""
+        for k, v in  json.items():
+            self.__dict__[k] = v
