59588ca26bd92c6fd21bbf4eb04ca04ee62d93dc
OBigVee <adedejivictor98@gmail.com>
2023-01-14 14:55:51 +0100

    :heavy_check_mark: task 1 completed
    - added 1-my_list.py file
    - file contains a derived class of list
    - class has just one function 'print_sorted()'
    - 'print_sorted() prints the list in ascending sort'
    - added 1-main.py test case file
    - added tests/1-my_list.txt doctest file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Class MyList that inherits from list"""
+
+
+class MyList(list):
+    """MyList class inherits from the list class"""
+
+    def print_sorted(self):
+        """method prints the list in ascending"""
+        return sorted(self)
