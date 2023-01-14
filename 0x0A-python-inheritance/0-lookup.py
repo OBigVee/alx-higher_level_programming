a79c9ca298a8c4c78f12ee85a476dbd054df7a4f
OBigVee <adedejivictor98@gmail.com>
2023-01-14 13:05:31 +0100

    :white_check_mark: task 0 completed

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function returns the list of available attributes and methods
+    of an object
+"""
+
+
+def lookup(obj):
+    """Args:
+        obj: object whose attributes and method to be return
+        
+        Returns:
+            list []
+    """
+    return [attr for attr in dir(obj)]
+
