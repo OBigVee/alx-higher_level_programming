afb14b250fec64023e503ada019124da94760846
OBigVee <adedejivictor98@gmail.com>
2023-05-18 15:48:40 +0100

    task 8 completed:heavy_check_mark:

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 --script displays the number of records with id=89 in the 
 -- table first_table of the DB hbtn_0c_0 in MySQL server
-SELECT  COUNT(id)  FROM first_table WHERE id=89;
+SELECT  COUNT(*)  FROM first_table WHERE id=89;
