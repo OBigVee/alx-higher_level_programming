c29e06531add59dc2a1242b3877c80dbca53d9da
OBigVee <adedejivictor98@gmail.com>
2023-05-18 15:46:26 +0100

    task 7 completed:heavy_check_mark:

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 --script inserts a new row in the table first_table 
 -- database(hbtn_0c_0) in MySQL sever
 
-INSERT INTO first_table(id, name) VALUES("89", "Best School");
+INSERT INTO first_table (id,name) VALUES(89,"Best School");
