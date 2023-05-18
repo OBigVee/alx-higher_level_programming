1f99e6d092f917ae70148fc242273a49d3a1b3a3
OBigVee <adedejivictor98@gmail.com>
2023-05-18 17:04:32 +0100

    task 9 done :heavy_check_mark:

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+--script creates a table second_table in the database
+-- hbtn_0c_0 in your MySQL server
+-- add multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), score INT);
+
+INSERT INTO second_table(id,name, score) VALUES(1, "john", 10);
+INSERT INTO second_table(id, name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table(id, name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table(id, name, score) VALUES(4,"Judge", 8);
+
