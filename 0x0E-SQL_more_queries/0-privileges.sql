1769559929d1b418e34892d5af422208cf0d04e2
OBigVee <adedejivictor98@gmail.com>
2023-05-20 09:26:07 +0100

    task 0 done :hevy_check_mark:

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- script list all privileges of the MySQ users user_0d_1 and user_0d_2 
-SHOW GRANTS FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
