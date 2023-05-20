cb8c42cf5266167780a5bb9b81206bf4537a2900
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:46:47 +0100

    task 5  completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,6 @@
 --script creates table unique_id 
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256))
+
+CREATE TABLE IF NOT EXISTS unique_id ( 
+        id   INT DEFAULT '1' UNIQUE,
+        name VARCHAR(256)
+    );
