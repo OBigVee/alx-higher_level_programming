632632f10cb3b774b159c2c4575824fa6e362e39
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:40:50 +0100

    task 1 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
 --script creates MySL server user user_0d_1
---user_0d_1 should have all privileges on your MySQL server
---The user_0d_1 password should be set to user_0d_1_pwd
---If the user user_0d_1 already exists, your script should not fai
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'  IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
