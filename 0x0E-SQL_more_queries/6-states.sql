74b652ce7c813ab38dc7fe040b7ad5ba32aef447
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:55:12 +0100

    task 6  completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
---script creates database hbtn_0d_usa and table states 
+-- creates the database `hbtn_0d_usa` and the table `states` (in the
+-- database `hbtn_0d_usa`) on a MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    PRIMARY KEY (id),
-    id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	PRIMARY KEY (id),
+	id	INT UNIQUE AUTO_INCREMENT	NOT NULL,
+	name	VARCHAR(256)			NOT NULL
 );
