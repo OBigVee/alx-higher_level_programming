cdde0cf8b0285097a5648a24da27bce63ee476d5
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:44:58 +0100

    task 4  completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,6 @@
---script creates table id_not_null 
+-- creates the table `id_not_null` on a MySQL server.
 
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id	INT DEFAULT '1',
+	name	VARCHAR(256)
+);
