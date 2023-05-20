c54a75f2ee910c5a37f946863907946d202cf4bf
OBigVee <adedejivictor98@gmail.com>
2023-05-20 13:19:35 +0100

    task 7 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 --script creates database hbtn_0d_usa and table cities 
 -- create db
-CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 --create table
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY(id),
     state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
