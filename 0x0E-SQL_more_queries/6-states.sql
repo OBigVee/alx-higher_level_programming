ec2115ed8271dd0254c0334185f0d765e18814de
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:54:14 +0100

    task 6  completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 --script creates database hbtn_0d_usa and table states 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     PRIMARY KEY (id),
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
