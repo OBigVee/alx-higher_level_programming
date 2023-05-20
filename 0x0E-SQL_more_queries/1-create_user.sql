b92b6b66baa8a02e5a66cf1c32f3f04300e6f9a7
OBigVee <adedejivictor98@gmail.com>
2023-05-20 12:41:30 +0100

    task 1 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
---script creates MySL server user user_0d_1
+-- creates new user `user_0d_1` with password `user_0d_1_pwd
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'  IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
