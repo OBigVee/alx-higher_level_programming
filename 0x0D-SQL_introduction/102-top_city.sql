d8ad0729b69364a7c995caade6236560b8c6bdaf
OBigVee <adedejivictor98@gmail.com>
2023-05-26 19:13:03 +0100

    task 102  completed :heavy_check_mark:

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,10 +2,10 @@
 -- and August ordered by temperature(DESC)
 WITH July_August AS (
 	SELECT *
-	FROM `temperatures`
-	WHERE `month` BETWEEN 7 AND 8
-) SELECT `city`, AVG(`value`) AS avg_temp
+	FROM temperatures
+	WHERE month BETWEEN 7 AND 8
+) SELECT city, AVG(value) AS avg_temp
 FROM July_August
-GROUP BY `city`
+GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
