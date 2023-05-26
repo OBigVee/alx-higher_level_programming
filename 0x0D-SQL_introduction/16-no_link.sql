5f33693d5c1bfc50124a42e195b25d0237307d15
OBigVee <adedejivictor98@gmail.com>
2023-05-26 18:04:12 +0100

    task 16  completed :heavy_check_mark:

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
---script that lists all records of the table second_table
+-- script that lists all records of the table second_table
 -- of the database hbtn_0c_0 in MySQL server
-SELECT score, name FROm second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
