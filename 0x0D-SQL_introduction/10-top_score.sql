08054f7a1d3d12ec9fa4df17e66e084918868932
OBigVee <adedejivictor98@gmail.com>
2023-05-26 17:48:20 +0100

    task 10  completed :heavy_check_mark:

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---script lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
-SELECT * FROM second_table ORDER BY score DESC;
+-- script lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC;
