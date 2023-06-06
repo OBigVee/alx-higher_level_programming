6df4969643214bc926bfc18505d9daeaeeb9a781
OBigVee <adedejivictor98@gmail.com>
2023-06-06 11:47:43 +0100

    task 8 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,4 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT * FROM cities WHERE state_id =(SELECT id FROM states WHERE name="California");
+SELECT * FROM cities WHERE state_id =(SELECT id FROM states WHERE name="California") ORDER BY id ASC;
