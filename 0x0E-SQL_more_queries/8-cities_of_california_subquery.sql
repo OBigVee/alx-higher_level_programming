f68942c6daae8d6a4bbfe71697793bf4e97f91ab
OBigVee <adedejivictor98@gmail.com>
2023-06-06 11:48:48 +0100

    task 8 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,5 +4,9 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 
-
-SELECT * FROM cities WHERE state_id =(SELECT id FROM states WHERE name="California") ORDER BY id ASC;
+SELECT id, name
+  FROM cities
+ WHERE state_id = ( SELECT id
+		      FROM states
+		     WHERE name = 'California')
+ ORDER BY id ASC;
