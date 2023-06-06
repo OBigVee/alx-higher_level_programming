92b2d86d8611ce1538440cc39c4cf04788e9f06a
OBigVee <adedejivictor98@gmail.com>
2023-06-06 11:00:38 +0100

    task 7 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
--- creates the database `hbtn_0d_usa` and the table `cities` (in the database `hbtn_0d_usa`) on a MySQL server.
+-- creates the database `hbtn_0d_usa` and the table `cities`
+-- (in the database `hbtn_0d_usa`) on a MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
