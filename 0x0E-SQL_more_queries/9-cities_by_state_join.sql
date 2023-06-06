acebcbe9069b70dcfad671fcd7d88c2846babeb7
OBigVee <adedejivictor98@gmail.com>
2023-06-06 12:59:07 +0100

    task 9 completed :heavy_check_mark:

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- script lists all cities contained in db htbn_0d_usa
 
-SELECT c.id AS 'id', c.name AS 'name', s.name AS 'name'
+SELECT *
   FROM cities AS c
        INNER JOIN states AS s
        ON c.state_id = s.id
