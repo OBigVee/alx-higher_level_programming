9b9b9fbab8bce4d098654d7cd98768b22c454df9
Victor Adedeji <adedejivictor98@gmail.com>
2023-06-14 05:59:16 +0100

    Update 1-multi_languages.js :heavy_check_mark:

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-/**
- * script prints 3 lines
- * "C is fun"
- * "Python is cool"
- * "Javascript is amazing"
- */
+
 console.log('C is fun');
 console.log('Python is cool');
-console.log('Javascript is amazing');
+console.log('JavaScript is amazing');
