b7d9a60975b65998562b3866aa4af168bf1c7dac
OBigVee <adedejivictor98@gmail.com>
2023-06-14 21:16:03 +0100

    task 5 completed :heavy_check_mark:

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,9 +3,7 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super();
-    this.height = size;
-    this.width = size;
-  }
+    constructor(size) {
+        super(size, size);
+    }
 };
