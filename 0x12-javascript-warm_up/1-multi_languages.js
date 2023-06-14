70fd9e1de9eab1472a8ec9021f41770bc645f175
OBigVee <adedejivictor98@gmail.com>
2023-06-14 04:31:17 +0100

    task 1 completed :heavy_check_mark:

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * script prints 3 lines
+ * "C is fun"
+ * "Python is cool"
+ * "Javascript is amazing"
+ */
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
