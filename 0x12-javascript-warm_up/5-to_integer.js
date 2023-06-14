2acace2c71ecc93fa850cb3046866ec51e77f92d
OBigVee <adedejivictor98@gmail.com>
2023-06-14 05:52:09 +0100

    Task 5 completed :heavy_check_mark:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,7 @@
  * My number :<first argument converted in integer>
  * if the first argument can be converted to an integer
  */
-const args = require('process').argv;
-const values = parseInt(args[2]);
+const values = parseInt(process.argv.slice(2));
 
 if (!isNaN(values)) {
   console.log(`My number: ${Number(values)}`);
