58b001e87a0f0d5bab7b6c62150a8294ccc906a0
OBigVee <adedejivictor98@gmail.com>
2023-06-14 21:00:48 +0100

    task 4 completed :heavy_check_mark:

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const a = this.height;
+    this.height = this.width;
+    this.width = a;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
