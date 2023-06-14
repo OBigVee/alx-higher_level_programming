4b9f037e990810134a56a13c02c0058311e3e27f
Victor Adedeji <adedejivictor98@gmail.com>
2023-06-14 05:57:18 +0100

    Update 5-to_integer.js :heavy_check_mark:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-/**
- * script prints
- * My number :<first argument converted in integer>
- * if the first argument can be converted to an integer
- */
-const values = parseInt(process.argv.slice(2));
 
-if (!isNaN(values)) {
-  console.log(`My number: ${values}`);
+const vals = parseInt(process.argv.slice(2));
+
+if (vals) {
+  console.log(`My number: ${vals}`);
 } else {
-  console.log('Not a member');
+  console.log('Not a number');
 }
