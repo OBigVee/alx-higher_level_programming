36a98f6a34edcee82a42c8543b83bfac1cf6182d
OBigVee <adedejivictor98@gmail.com>
2023-06-26 18:06:58 +0100

    task 2 completed :heavy_check_mark:

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches
+the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+    NAME_SEARCHED = str(sys.argv[4])
+    print(NAME_SEARCHED)
+    PORT = 3306
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=USERNAME, passwd=PASSWORD,
+        db=DATABASE_NAME
+    )
+    cur = db.cursor()
+
+    cur.execute(
+        f"SELECT * FROM {DATABASE_NAME}.states WHERE BINARY name = %s\
+                 ORDER BY states.id ASC",
+        (NAME_SEARCHED,),
+    )
+    for row in cur.fetchall():
+        print(row)
