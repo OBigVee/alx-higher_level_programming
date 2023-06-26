cdd7cbe9274057f800dd3805d512b7e357c933b3
OBigVee <adedejivictor98@gmail.com>
2023-06-26 19:05:11 +0100

    task 3 completed :heavy_check_mark:

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches
+the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+    NAME_SEARCHED = sys.argv[4]
+    PORT = 3306
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=USERNAME, passwd=PASSWORD,
+        db=DATABASE_NAME
+    )
+    cur = db.cursor()
+    cur.execute(
+        f"SELECT * FROM {DATABASE_NAME}.states WHERE BINARY name = %s\
+              ORDER BY states.id ASC", (NAME_SEARCHED,))
+    for row in cur.fetchall():
+        print(row)
