683169d73e2dcee898fec13191f3469d66cc42cf
OBigVee <adedejivictor98@gmail.com>
2023-06-27 21:15:50 +0100

    task 5 completed :heavy_check_mark:

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+script lists all cities from db hbtn_0e-04_usa
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+    STATE_NAME = sys.argv[4]
+    PORT = 3306
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=USERNAME, passwd=PASSWORD,
+        db=DATABASE_NAME
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.name\
+        FROM cities INNER JOIN states on cities.state_id=states.id\
+        WHERE states.name=%s ORDER BY cities.id ASC",
+        (STATE_NAME,),
+    )
+    for row in cur.fetchall():
+        for i in row:
+            print(f"{i}, ", end="")
+    print()
+    cur.close()
+    db.close()
