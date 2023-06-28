a325b63419589f224dfc1ff418458e7fe0a3b4f4
OBigVee <adedejivictor98@gmail.com>
2023-06-28 17:53:51 +0100

    task 7 completed :heavy_check_mark:

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,49 +4,33 @@ script list all State objects from the
 database hbtn_0e_6_usa
 """
 
-# if __name__ == "__main__":
-
-#     import sys
-#     from sqlalchemy import create_engine
-#     from sqlalchemy.orm import sessionmaker
-#     from sqlalchemy import URL
-
-#     from model_state import State, Base
-
-#     USERNAME = sys.argv[1]
-#     PASSWORD = sys.argv[2]
-#     DATABASE_NAME = sys.argv[3]
-#     PORT = 3306
-
-#     url_object = URL.create(
-#         "mysql+mysqldb",
-#         username="root",
-#         password="thisme",
-#         host="localhost",
-#         database="hbtn_0e_6_usa",
-#     )
-
-#     engine = create_engine(url=url_object, pool_pre_ping=True)
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-
-#     for row in session.query(State).all():
-#         print(f"{row.id}: {row.name}")
-#     session.close()
 if __name__ == "__main__":
+
     import sys
-    from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import URL
 
-    _, user, passwd, db = sys.argv
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(user, passwd, db),
-        pool_pre_ping=True,
+    from model_state import State, Base
+
+    USERNAME = sys.argv[1]
+    PASSWORD = sys.argv[2]
+    DATABASE_NAME = sys.argv[3]
+    PORT = 3306
+
+    url_object = URL.create(
+        "mysql+mysqldb",
+        username="root",
+        password="thisme",
+        host="localhost",
+        database="hbtn_0e_6_usa",
     )
+
+    engine = create_engine(url=url_object, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).all():
-        print("{}: {}".format(state.id, state.name))
+    for row in session.query(State).all():
+        print(f"{row.id}: {row.name}")
     session.close()
+
