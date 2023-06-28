f3eff092abbbbe2cc4d21934d3b5bb0dca9ae7fc
OBigVee <adedejivictor98@gmail.com>
2023-06-28 11:18:16 +0100

    task 6 completed :heavy_check_mark:

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,30 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy import URL
 
-# import MySQLdb
-
-# conn = MySQLdb.connect(
-#     host="localhost",
-#     port=3036,
-
-# )
-
-url_object = URL.create(
-    "mysql+mysqldb",
-    username="root",
-    password="thisme",
-    host="localhost",
-    database="hbtn_0e_6_usa",
-)
-
 Base = declarative_base()
-
 
 class State(Base):
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-# engine = create_engine(url_object)
