7cb614a62ebbfd3a60564892f1a03e3199e00909
OBigVee <adedejivictor98@gmail.com>
2023-06-29 12:49:50 +0100

    task 9 completed :heavy_check_mark:

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,49 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import create_engine
-    from model_state import State, Base
+# if __name__ == "__main__":
+#     import sys
+#     from sqlalchemy.orm import sessionmaker
+#     from sqlalchemy import create_engine
+#     from model_state import State, Base
     
-    username = sys.argv[1]
-    password = sys.argv[2]
-    DB = sys.argv[3]
+#     username = sys.argv[1]
+#     password = sys.argv[2]
+#     DB = sys.argv[3]
 
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{DB}"
-    )
+#     engine = create_engine(
+#         f"mysql+mysqldb://{username}:{password}@localhost:3306/{DB}"
+#     )
 
-    #Base.metadata.bind = engine
+#     #Base.metadata.bind = engine
 
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
+
+#     # states_a = session.query(State).filter(State.name.like("%a%")).order_by(
+#     #     State.id.asc()).all()
+
+#     #if states_a:
+#     for state in session.query(State).filter(State.name.like("%a%")):
+#         print(f"{state.id}: {state.name}")
+#     # else:
+#     #     print("Nothing")
+#     # session.close()
+#!/usr/bin/python3
+"""Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
+
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    _, user, passwd, db = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user,
+                           passwd, db))
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # states_a = session.query(State).filter(State.name.like("%a%")).order_by(
-    #     State.id.asc()).all()
-
-    #if states_a:
-    for state in session.query(State).filter(State.name.like("%a%")):
-        print(f"{state.id}: {state.name}")
-    # else:
-    #     print("Nothing")
-    # session.close()
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print('{}: {}'.format(state.id, state.name))
