274884498c9a975dff423fcf97ef4861bb6bd88d
OBigVee <adedejivictor98@gmail.com>
2023-06-29 19:36:04 +0100

    task 14 completed :heavy_check_mark:

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script prints all city objects from the
+database hbtn_0e_14_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import State
+    from model_city import Base, City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+
+    _, username, passwd, db = sys.argv
+
+    url = f"mysql+mysqldb://{username}:{passwd}@localhost:3306/{db}"
+    engine = create_engine(url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).join(State).order_by(City.id).all()
+
+    for city in cities:
+        print(f"{city.state.name}: ({city.id} {city.name})")
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""city class"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """city class"""
+
+    __tablename__ = "cities"
+
+    id = Column(
+        Integer, primary_key=True,
+        autoincrement=True, unique=True, nullable=False
+    )
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    state = relationship("State", back_populates="cities")
