a3556ac8b084c40bcbfa5f36e434089c3774d8c3
OBigVee <adedejivictor98@gmail.com>
2023-06-29 12:23:17 +0100

    task 9 completed :heavy_check_mark:

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import State, Base
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    DB = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{DB}"
+    )
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_a = session.query(State).filter(State.name.like("%a%")).all()
+
+    if states_a:
+        for state in states_a:
+            print(f"{state.id}: {state.name}")
+    else:
+        print("Nothing")
+    session.close()
