cb176c9d53926999920dad430a2ef92e16b238fe
OBigVee <adedejivictor98@gmail.com>
2023-07-02 16:10:35 +0100

    task 0 completed :heavy_check_mark:

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line argument
-url=$1
-
-# Send a request to the URL and store the response in a variable
-response=$(curl -s -X GET -I "$url")
-
-# Extract the content length from the response headers
-content_length=$(echo "$response" | grep -i -e "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-# Display the size of the response body in bytes
-echo "$content_length"
+# script sends a request to URL and displays the size of the response body
+curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
