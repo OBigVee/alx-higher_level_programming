c510eb1b40b1f39b97c662874a76c244b7a0c037
OBigVee <adedejivictor98@gmail.com>
2023-07-02 18:27:15 +0100

    task 5 completed :heavy_check_mark:

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,5 @@
 # Bash script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 # A variable email must be sent with the value test@gmail.com
 # A variable subject must be sent with the value I will always be here for PLD
-curl -L -X POST -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
+# curl -L -X POST -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
