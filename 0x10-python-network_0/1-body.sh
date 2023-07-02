64889ff2f0a4b164eea8cf9468d7d13e1eecdbf7
OBigVee <adedejivictor98@gmail.com>
2023-07-02 17:29:15 +0100

    task 1 completed :heavy_check_mark:

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,4 +2,14 @@
 # script takes a url and send a 
 # GET request to the URL and
 # displays the body of the response
-curl -s -L "$1"
+#curl -s -L "$1"
+
+# Check if URL is provided as argument
+if [ $# -eq 0 ]; then
+  echo "Please provide a URL as an argument."
+  exit 1
+fi
+
+# Send GET request and display the response body
+response=$(curl -s -L "$1")
+echo "$response"
