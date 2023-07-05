610ce44c405a10ddb88d2a4a887feb3e61d537c4
OBigVee <adedejivictor98@gmail.com>
2023-07-05 14:58:16 +0100

    task 6 completed :heavy_check_mark:

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,21 +13,24 @@ def find_peak(list_of_integers):
     if n == 0:
         return peak
 
-    # check the first element
-    if n >= 1 and list_of_integers[0] >= list_of_integers[1]:
-        peak = list_of_integers[0]
-        return peak
+    # # check the first element
+    # if n >= 1 and list_of_integers[0] >= list_of_integers[1]:
+    #     peak = list_of_integers[0]
+    #     return peak
 
-    # check the last element
-    if n >= 2 and list_of_integers[n - 1] >= list_of_integers[n - 2]:
-        peak = list_of_integers[n - 1]
-        return peak
+    # # check the last element
+    # if n >= 2 and list_of_integers[n - 1] >= list_of_integers[n - 2]:
+    #     peak = list_of_integers[n - 1]
+    #     return peak
 
-    # iterate through the rest of list_of_integer
-    for i in range(1, n - 1):
-        if (
-            list_of_integers[i] >= list_of_integers[i - 1]
-            and list_of_integers[i] >= list_of_integers[i + 1]
-        ):
-            peak = list_of_integers[i]
-            return peak
+    # # iterate through the rest of list_of_integer
+    # for i in range(1, n - 1):
+    #     if (
+    #         list_of_integers[i] >= list_of_integers[i - 1]
+    #         and list_of_integers[i] >= list_of_integers[i + 1]
+    #     ):
+    #         peak = list_of_integers[i]
+    #         return peak
+    list_of_integers.sort()
+    peak = list_of_integers[-1]
+    return peak
