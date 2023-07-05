1680d51610c2c85594833e74ad7c8995677797ba
obigvee <adedejivictor98@gmail.com>
2023-07-05 06:41:40 -0700

    task 4  completed :heavy_check_mark:

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# script takes in a URL as an argument, send a 
-# GET request to the URL
-# and displays the body ofthe response
-# curl --get -H "X-School-User-Id: 98" "$1"
+# script takes in a URL as an argument, send a GET request to the URL and displays the body ofthe response curl --get -H "X-School-User-Id: 98" "$1"
 curl -s -H "X-School-User-Id: 98" "$1"
