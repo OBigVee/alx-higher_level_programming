1a3d1b0316dc01927143f42272b0694c708748e3
OBigVee <adedejivictor98@gmail.com>
2023-07-06 16:07:13 +0100

    task 0 completed :heavy_check_mark:

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,10 @@
 """python script fetches url"""
 import urllib.request
 
-
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    the_page = response.read()
-    print("Body response:")
-    print("\t - type: {}".format(type(the_page)))
-    print("\t - content: {}".format(the_page))
-    print("\t - utf8 content: {}".format(the_page.decode("utf-8")))
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as r:
+    html = r.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
