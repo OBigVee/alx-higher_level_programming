2244662bd0d7da8695f5d9b7c6f30952372a8d4e
OBigVee <adedejivictor98@gmail.com>
2023-07-06 18:58:46 +0100

    task 7 completed :heavy_check_mark:

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if r.status_code == 200:
+    try:
+        r.status_code == 200
         print(r.text)
-    else:
+    except Exception as e:
         print(f"Error code: {r.status_code}")
