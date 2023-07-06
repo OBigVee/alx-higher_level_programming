2746cbaa66fce7a0fe8d001086c7b0af88cbe562
OBigVee <adedejivictor98@gmail.com>
2023-07-06 16:41:24 +0100

    task 1 completed :heavy_check_mark:

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python scrip takes a URL and sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as res:
+        print(f"{res.getheader('X-Request_id')}")
