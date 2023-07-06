aa0eb266840c2c689a408141cc63267ea3da1759
OBigVee <adedejivictor98@gmail.com>
2023-07-06 15:40:06 +0100

    task 0 completed :heavy_check_mark:

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script fetches url"""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(the_page)))
+        print("\t - content: {}".format(the_page))
+        print("\t - utf8 content: {}".format(the_page.decode("utf-8")))
