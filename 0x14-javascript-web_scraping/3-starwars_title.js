d436c131761e4ca7b8968a38f4401a3de5109d34
OBigVee <adedejivictor98@gmail.com>
2023-07-26 23:39:31 +0100

    task 3  done :heavy_check_mark:

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 /**
- * Script prints the title of a Star Wars movie where the 
+ * Script prints the title of a Star Wars movie where the
  * episode number matches a given integer
  */
 
 const request = require('request');
 
 if (process.argv.length !== 3) {
-    process.exit();
+  process.exit();
 }
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
-request(url, function(err, r, body) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(JSON.parse(body).title);
-    }
-})
+request(url, function (err, r, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
