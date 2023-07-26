c6e2121c583c399144a8a5f02b09d33ad002e505
OBigVee <adedejivictor98@gmail.com>
2023-07-27 00:35:46 +0100

    task 5  done :heavy_check_mark:

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,19 +8,19 @@ const request = require('request');
 const fs = require('fs');
 
 if (process.argv.length !== 4) {
-  process.exit();
+    process.exit();
 }
 const [url, path] = process.argv.slice(2);
 
-request(url, function (err, resp, body) {
-  if (err) {
-    console.log(err);
-    process.exit();
-  }
-  const data = JSON.parse.body();
-  fs.writeFileSync(path, data, (err) => {
+request(url, function(err, resp, body) {
     if (err) {
-      console.log(err);
+        console.log(err);
+        process.exit();
     }
-  });
+    const data = JSON.parse(body);
+    fs.writeFileSync(path, data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
