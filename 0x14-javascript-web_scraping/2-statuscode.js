205c42a8ed3f69d6ea5e15625d3f032e7f83e780
OBigVee <adedejivictor98@gmail.com>
2023-07-26 22:38:52 +0100

    task 2 done :heavy_check_mark:

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/**
+ * script displays the status code of a GET request
+ */
+
+const request = require('request');
+
+if (process.argv < 3) {
+    process.exit();
+}
+
+url = process.argv[2];
+request.get(url).on('response', r => {
+    console.log(`code:${r.statusCode}`)
+})
