ae6ee6b84233ee6e48a84c806e8ab4e78734f052
OBigVee <adedejivictor98@gmail.com>
2023-07-26 22:24:47 +0100

    task 1 done :heavy_check_mark:

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,23 @@
 #!/usr/bin/node
+
 /**
-* script writes a string to a file
-*
-*/
+ * script writes a string to a file
+ *
+ */
 
 const fs = require('fs');
+const { encode } = require('punycode');
 
-if (process.argv > 2){
-	fs.writefile()
+if (process.argv < 3) {
+    process.exit();
 }
+file = process.argv[2];
+data = process.argv[3];
+
+fs.writeFile(file, data, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(fs.readFileSync(file, 'utf-8'))
+    }
+})
