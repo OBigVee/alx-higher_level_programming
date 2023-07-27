fa0738af7d9a528a9e536332a8932066f45be73e
OBigVee <adedejivictor98@gmail.com>
2023-07-27 12:01:28 +0100

    task 7  done :heavy_check_mark:

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,33 +2,31 @@
 
 /**
  * script prints all characters of a star wars movie
- * 
+ *
  */
 const request = require('request');
 
 if (process.argv.length !== 3) {
-    process.exit();
+  process.exit();
 }
 
-
 const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`
-request(url, function(err, res, body) {
-    if (err) {
-        console.log(err);
-    }
-    if (res.statusCode === 200) {
-        const data = JSON.parse(body);
-        for (const char of data.characters) {
-            request(char, function(err, res, body) {
-                if (err) {
-                    console.log(err);
-                } else if (res.statusCode === 200) {
-                    const name = JSON.parse(body);
-                    console.log(name.name);
-                }
-            })
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  if (res.statusCode === 200) {
+    const data = JSON.parse(body);
+    for (const char of data.characters) {
+      request(char, function (err, res, body) {
+        if (err) {
+          console.log(err);
+        } else if (res.statusCode === 200) {
+          const name = JSON.parse(body);
+          console.log(name.name);
         }
+      });
     }
-
-})
+  }
+});
