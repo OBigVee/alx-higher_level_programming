882f7ec257afc372324d42f3a8d3ea27d6e89e13
OBigVee <adedejivictor98@gmail.com>
2023-07-27 10:49:12 +0100

    task 5  done :heavy_check_mark:

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,27 +8,8 @@ const request = require('request');
 const fs = require('fs');
 
 if (process.argv.length !== 4) {
-    process.exit();
+  process.exit();
 }
 const [url, path] = process.argv.slice(2);
 
-// request(url, function(err, resp, body) {
-//     if (err) {
-//         console.log(err);
-//         process.exit();
-//     }
-//     // check if the response content type is json
-//     const contentType = resp.headers['content-type'];
-//     try {
-//         const data = JSON.parse(body);
-//         fs.writeFileSync(path, data, (err) => {
-//             if (err) {
-//                 console.log(err);
-//             }
-//         });
-//     } catch (parseErr) {
-//         console.log("Error parsing JSON:", parseErr);
-//     }
-
-// });
 request(url).pipe(fs.createWriteStream(path));
