5ffb000459fe0ca9ef9fa7b602b3bb1eba13c767
OBigVee <adedejivictor98@gmail.com>
2023-07-27 11:38:11 +0100

    task 6  done :heavy_check_mark:

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,25 +7,26 @@
 const request = require('request');
 
 if (process.argv.length !== 3) {
-  process.exit();
+    process.exit();
 }
 
 const url = process.argv[2];
-request(url, (err, res, body) => {
-  const compSum = {};
-  if (err) {
-    process.exit();
-  } else
-  if (res.statusCode === 2000) {
-    const todoList = JSON.parse(body);
-    for (const task of todoList) {
-      if (task.completed) {
-        if (compSum[task.userId] === undefined) {
-          compSum[task.userId] = 0;
+
+request(url, function(err, res, body) {
+    const compSum = {};
+
+    if (err) {
+        console.log(err);
+    } else if (res.statusCode === 2000) {
+        const todoList = JSON.parse(body);
+        for (const task of todoList) {
+            if (task.completed) {
+                if (compSum[task.userId] === undefined) {
+                    compSum[task.userId] = 0;
+                }
+                compSum[task.userId] += 1;
+            }
         }
-        compSum[task.userId] += 1;
-      }
+        console.log(compSum);
     }
-    console.log(compSum);
-  }
 });
