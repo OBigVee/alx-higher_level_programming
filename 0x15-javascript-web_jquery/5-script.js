0a6b8901421d1059f052864cfebc8a28315cf130
OBigVee <adedejivictor98@gmail.com>
2023-08-03 01:35:22 +0100

    task 5 :heavy_check_mark:

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,12 @@
+/**
+ * Write a JavaScript script that adds a <li> element to a list
+ * when the user clicks on the tag DIV#add_item
+ * The new element must be: <li>Item</li>
+ * The new element must be added to UL.my_list
+ * You can’t use document.querySelector to select the HTML tag
+ * You must use the JQuery API
+ */
+$('div#add_item').click(
+  function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
